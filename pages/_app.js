3b955b4bc2ad29ce20cf1d69b2c4550ffca95d3b
Miranda <m.naranjo.medel@gmail.com>
2026-05-24 17:52:49 -0400

    se agrega favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 // pages/_app.js
 import '../styles/globals.css';
 import React from 'react';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/icono_1.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
